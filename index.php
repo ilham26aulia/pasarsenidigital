54a97cc881480cd7bb4e3507d491d1d818b2da94
ilham <ilham.pembangunan@gmail.com>
2015-05-22 07:44:20 +0700

    initial released

---
+++ index.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="../../favicon.ico">
+
+        <title>Pasar Seni Digital</title>
+        <link href="asset/css/bootstrap.min.css" rel="stylesheet">
+        <link href="asset/css/psd.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" class="active" ></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                    <li data-target="#myCarousel" data-slide-to="3"></li>
+                    <li data-target="#myCarousel" data-slide-to="4"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox" >
+                    <div class="item active">
+                        <div class="psd-big-logo"></div>
+                        <div class="carousel-caption">
+                            <p class="title">Kenali kemampuanmu, mulailah berkarya, dan jual kreasi produk digitalmu bersama kami di<br></p>
+                            <h2 style="margin-top: 0;">PasarSeniDigital.com</h2>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="row">
+                            <div class="col-sm-2"></div>
+                            <div class="col-sm-5" style="margin-top: 20%;">
+                                <i>Jelajahi dan Temukan</i>
+                                <p class="title">KARENA KARYA TIDAK MAMPU BERBICARA SENDIRI</p>
+                                <p><strong>PasarSeniDigital.com(PSD)</strong> adalah cara mudah untuk berbagi karya,
+                                    menjualnya dan menjadikannya mudah untuk ditemukan</p>
+                            </div>
+
+                            <div class="col-sm-4">
+                                <div class="img-roket"></div>
+                            </div>
+                            <div class="col-sm-1"></div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="row">
+                            <div class="col-sm-1"></div>
+                            <div class="col-sm-5">
+                                <div class="img-atmosphere"></div>
+                            </div>
+                            <div class="col-sm-5" style="margin-top: 20%;">
+                                <i>Tak Perlu Menjadi Seorang Jenius</i>
+                                <p class="title">KREATIFITAS BUKAN BAKAT TAPI SEBUAH PROSES</p>
+                                <p>Menciptakan sesuatu adalah proses panjang yang tidak menentu.
+                                    Seorang "Kreator" harus menunjukkan karyanya.<br>Berbagilah dengan apa yang kamu miliki 
+                                    kepada banyak orang, karena bisa jadi itu sangat berguna.</p>
+                            </div>
+                            <div class="col-sm-1"></div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <div class="row">
+                            <div class="col-sm-1"></div>
+                            <div class="col-sm-5" style="margin-top: 20%;margin-left: 25px;">
+                                <i>Cara Mudah Untuk Berbagi, Menjual atau Membeli Konten Digital</i>
+                                <p class="title">FILE ATAU DOKUMEN DESAIN PURWARUPA</p>
+                                <p><strong>PasarSeniDigital.com(PSD)</strong> adalah tempat dimana kita bisa menjual
+                                    atau membeli semua bentuk kreasi produk berbasis digital seperti : 
+                                    foto atau gambar, theme atau template (jomla, wordpress, html dll), brosur, kartu nama,
+                                    presentasi, </p>
+                            </div>
+
+                            <div class="col-sm-5">
+                                <div class="img-book"></div>
+                            </div>
+                            <div class="col-sm-1"></div>
+                        </div>
+                    </div>
+                    <div class="item"  style="text-align: center;">
+                        <!--div class="bg-gradient">
+                            <form class="navbar-form" role="search">
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                </div>
+                                <button type="submit" class="btn btn-default">Submit</button>
+                            </form>
+                        </div-->
+                        <p class="title" style="margin-top: 75px;">MENJADI KREATIF BUKAN SEKEDAR HOBI TETAPI CARA UNTUK HIDUP</p>
+                        <p>
+                            Mari bergabung bersama kami, buka tokomu sendiri dan jual produk digitalmu hanya di<br>
+                            <strong>PasarSeniDigital.com</strong>
+                        </p>
+                        <div>
+                            <br>
+                            <button class="invite-button">INVITE</button>
+                        </div>
+                        <div style="margin-top: 10px;">
+                            <a href="" target="_blank"><img src="asset/img/fb.png"></a>
+                            <a href="" target="_blank"><img src="asset/img/twitter.png"></a>
+                            <a href="" target="_blank"><img src="asset/img/gplus.png"></a>
+                            <a href="" target="_blank"><img src="asset/img/youtube.png"></a>
+                        </div>
+                    </div>
+                    <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                        <span class="glyphicon arrow-slide-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                        <span class="glyphicon arrow-slide-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div><!-- /.carousel -->
+        </div>
+        <footer class="footer">
+            <div class="container">
+                <div class="psd-small-logo pull-left"></div>
+                <div class="pull-right"><small><br>Copyright &COPY;2015 pasarsenidigital.com</small></div>
+            </div>
+        </footer>
+        <script src="asset/js/jquery-2.1.4.min.js"></script>
+        <script src="asset/js/bootstrap.min.js"></script>
+    </body>
+</html>
